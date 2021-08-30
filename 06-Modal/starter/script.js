1bcf0b6f1b79f277575d0994e0ccdd5ff488326b
chinmay <chinmaybs89@gmail.com>
2021-08-30 19:54:40 +0530

    complete modal building
    adding and removing classes using javascript

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//use classes when adding and removing styles
+//avoid using induvidual styles
+//key press events belong to document itself and parameter is passed ction event
+// console.log(document.contentType);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  console.log('button clicked');
+  modal.classList.remove('hidden'); //dont use dot
+  overlay.classList.remove('hidden');
+};
+// console.log(btnCloseModal, btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  //console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+// console.log(document);
+document.addEventListener('keydown', function (event) {
+  //console.log(event);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+    closeModal();
+});
